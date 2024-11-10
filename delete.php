5c1de195de34daf887bdc55070dccd3eadd3450b
Firoj069 <169171165+MdFirojulIslam@users.noreply.github.com>
2024-11-11 01:35:13 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,20 +1,19 @@
 <?php
+	spl_autoload_register(function($class){
+		include $class .'.php';
+	});
+	$person = new Person();
 
-    spl_autoload_register(function($class){
-        include $class .'.php';
-    });
-
-    $user = new Person();
-
-     if (isset($_GET['action']) && $_GET['action']=='delete')
-    {
-        $id = (int)$_GET['id'];
-        if ($user->delete($id)) 
-        {
-            echo "<script> alert('Deleted successfully');
-                        window.location.href = 'index.php';
-                   </script>";
-                exit();
-        }
-    }
+	if (isset($_GET['action']) && $_GET['action']=='delete') 
+	{
+		$id = (int)$_GET['id'];
+		
+		if ($person->delete($id)) 
+		{
+			echo "<script>
+					alert('deleted successfully');
+					window.location.href = 'show.php';
+			</script>";
+		}
+	}
 ?>
